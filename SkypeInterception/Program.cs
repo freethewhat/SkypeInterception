cd5897c3676fa5bbf08d30b740d317027328d48c
Matt Sutton <mcsutton@outlook.com>
2021-03-25 17:39:40 -0600

    Add project files.

---
+++ SkypeInterception/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace Skype_to_Dialer
+{
+    class SkypeDialer
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string checkme = args[0];
+                if (checkme.CompareTo("--") == 0)
+                {
+                    checkme = args[1];
+                }
+                string pattern = @"\d+";
+                Match m = Regex.Match(checkme, pattern, RegexOptions.IgnoreCase);
+                if (m.Success)
+                {
+                    Console.WriteLine(m.Groups[0].Value);
+                    var url = "tel:" + m.Groups[0].Value;
+                    var psi = new ProcessStartInfo();
+                    psi.UseShellExecute = true;
+                    psi.FileName = url;
+
+                    Process.Start(psi);
+                }
+            }
+        }
+
+    }
+}
